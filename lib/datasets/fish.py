d73c1c20c3ea3c91dbbbdeecab38e28bb8683fda
Dmitry <dmitrybaranchuk@gmail.com>
2017-02-03 21:30:50 +0300

    remove bait

--- lib/datasets/fish.py
+++ lib/datasets/fish.py
@@ -26,7 +26,7 @@ class fish(imdb):
         self._devkit_path = devkit_path
         self._data_path = os.path.join(self._devkit_path, 'data')
         self._classes = ('__background__', # always index 0
-                         'ALB', 'BET', 'DOL', 'LAG', 'OTHER', 'SHARK', 'YFT', 'BAIT')
+                         'ALB', 'BET', 'DOL', 'LAG', 'OTHER', 'SHARK', 'YFT')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = ['.jpg']
         self._image_index = self._load_image_set_index()
