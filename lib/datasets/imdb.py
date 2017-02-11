11130e84a0172efe2829a9a8243406605ba180e5
Dmitry <dmitrybaranchuk@gmail.com>
2017-02-12 00:13:06 +0300

    .

--- lib/datasets/imdb.py
+++ lib/datasets/imdb.py
@@ -117,7 +117,7 @@ class imdb(object):
             # num_images - len(dublicated_names) > 1722 means that
             # we augment 5 ALB images to finally get 5000 images
             if 1 in self.roidb[i]['gt_classes'] and \
-               num_images - len(dublicated_names) > 1722:
+               num_images - len(dublicated_names) > 1720:
                 continue
             dublicated_names.append(self._image_index[i])
             # Main part
