758614f1d8ef98dc2ad2b5ef06f1a47cae40eeaa
Dmitry <dmitrybaranchuk@gmail.com>
2017-02-15 16:06:18 +0300

    add rotations

--- lib/datasets/imdb.py
+++ lib/datasets/imdb.py
@@ -121,6 +121,7 @@ class imdb(object):
             w = widths[i]
 
             boxes = self.roidb[i]['boxes'].copy()
+            print (boxes[:2])
             _boxes = boxes.reshape((len(boxes), 2, 2))
             # centering coords
             _boxes[:, :, 0] = _boxes[:, :, 0] - w/2
@@ -135,6 +136,7 @@ class imdb(object):
             _boxes[_boxes[:, 1, 0]>w-1] = w-1
             _boxes[_boxes[:, 1, 1]>h-1] = h-1
             boxes = _boxes.reshape((len(boxes), 4))
+            print (boxes[:2])
             assert (boxes[:, 2] >= boxes[:, 0]).all()
             entry = {'boxes' : boxes,
                      'gt_overlaps' : self.roidb[i]['gt_overlaps'],
