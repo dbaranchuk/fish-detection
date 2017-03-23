128131803cf6a09620b24a881032f91169092a91
Dmitry <dmitrybaranchuk@gmail.com>
2017-03-23 17:52:16 +0300

    .

--- lib/roi_data_layer/minibatch.py
+++ lib/roi_data_layer/minibatch.py
@@ -178,7 +178,8 @@ def _get_bbox_regression_labels(bbox_target_data, num_classes):
     clss = bbox_target_data[:, 0].astype(np.int64)
     bbox_targets = np.zeros((clss.size, 4 * num_classes), dtype=np.float32)
     bbox_inside_weights = np.zeros(bbox_targets.shape, dtype=np.float32)
-    inds = np.where(clss > 0)[0].astype(int)
+    inds = np.where(clss > 0)[0]
+    print inds
     for ind in inds:
         cls = clss[ind]
         start = 4 * cls
